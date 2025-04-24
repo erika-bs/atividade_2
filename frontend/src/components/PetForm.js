4420c720222726d5a6a7b465a16928a3c12d6140
erika-bs <167558855+erika-bs@users.noreply.github.com>
2025-04-24 12:07:59 -0300

    Update PetForm.js

--- frontend/src/components/PetForm.js
+++ frontend/src/components/PetForm.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import api from "../service/api";
 
-// Estilos com Styled Components
 const FormContainer = styled.form`
   background-color: #fff;
   padding: 20px;
